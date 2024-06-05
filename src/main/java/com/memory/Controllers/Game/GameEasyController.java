db9d2837b5ef26d4734f95ca602a87c193afbde3
Arthur <arthurcalazans@gmail.com>
2024-06-05 14:52:44 -0300

    Buttons of Board Created

--- src/main/java/com/memory/Controllers/Game/GameEasyController.java
+++ src/main/java/com/memory/Controllers/Game/GameEasyController.java
@@ -1,6 +1,7 @@
 package com.memory.Controllers.Game;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
@@ -10,9 +11,24 @@ import java.util.ResourceBundle;
 public class GameEasyController implements Initializable {
     public GridPane gridPane;
     public Label time_lbl;
+    public Button[][] buttons;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        this.buttons = new Button[gridPane.getRowCount()][gridPane.getColumnCount()];
+        createButtons();
     }
+
+    public void createButtons() {
+        for (int i = 0; i < gridPane.getRowCount(); i++) {
+            for (int j = 0; j < gridPane.getColumnCount(); j++) {
+                buttons[i][j] = new Button();
+                gridPane.add(buttons[i][j], j, i);
+                buttons[i][j].setMinWidth(117.6);
+                buttons[i][j].setMinHeight(68);
+            }
+        }
+    }
+
+
 }
